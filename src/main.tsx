36fab93340a89bee4299a9896048a78d65fad127
AndyML13 <pescherov13@gmail.com>
2024-08-06 12:55:28 +0300

    add paths to manifest ton json

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react';
 // this manifest is used temporarily for development purposes
 // const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
 
-const manifestUrl = 'https://andyml13.github.io/tonProject/public/tonconnect-manifest.json';
+const manifestUrl = 'https://andyml13.github.io/first_ton_project/public/tonconnect-manifest.json';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
